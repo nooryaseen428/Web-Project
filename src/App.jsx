4eb418d7a2c714348a48bda81b33023ab25592c3
nooryaseen428 <s12323629@stu.najah.edu>
2024-12-23 22:02:02 +0200

    git publish

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-=import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import CardiologySection from './components/CardiologySection';
 import 'bootstrap/dist/css/bootstrap.min.css';
